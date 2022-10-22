1f3a50e4eed94865573a46589e7dc8dd3c057521
Akshay Kumar <akshay08022002@gmail.com>
2022-10-22 16:15:47 +0530

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by AKSHAY KUMAR on 22/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
